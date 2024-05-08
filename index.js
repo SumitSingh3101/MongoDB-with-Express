048c284bcd99b35b4b2fe5ac9d9ed1a0915f8032
SumitSingh3101 <sumitsingh310106@gmail.com>
2024-05-08 20:34:52 +0530

    Learning MongoDB curd oprations with express

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const Chat = require("./models/chat.js");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({extended: true}));
 
 main()
 .then(() => {
@@ -30,4 +31,27 @@ app.listen(8080, () => {
 
 app.get("/", (req,res) => {
     res.send("root is working");
+});
+
+app.get("/chats/new", (req, res) => {
+    res.render("new.ejs");
+});
+
+//Create Route
+app.post("/chats",(req,res) => {
+    let {from, to, msg} = req.body;
+    let newChat = new Chat({
+        from: from,
+        to: to,
+        msg: msg,
+        created_at: new Date(),
+    });
+
+    newChat.save().then((res)=>{
+        console.log("chat saved");
+    }).catch((err) => {
+        console.log(err);
+    });
+    
+    res.redirect("/chats");
 });
